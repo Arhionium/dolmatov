ebe57c7e2caf815c6ac0b38fd4f201dda6474bc2
Nikita <you@example.com>
2023-10-03 19:28:32 +0500

    Создал Тему 9

--- Tema8_1.py
+++
@@ -1,9 +0,0 @@
-class Animal:
-    def __init__(self, name, weight):
-        self.name = name
-        self.weight = weight
-
-my_animal = Animal('Sobaka', 7)
-
-print(my_animal.name)
-print(my_animal.weight)
